145576312bb62e0b8995b45450f9d081a7acae83
seasameoil <whdwlgus12@gmail.com>
2023-01-19 19:56:06 +0900

    first commit

---
+++ src/main/java/project/pill_solution/PillSolutionApplication.java
@@ -0,0 +1,13 @@
+package project.pill_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillSolutionApplication.class, args);
+	}
+
+}
